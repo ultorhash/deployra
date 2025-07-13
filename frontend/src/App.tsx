a624f6682d65267e1b48742928bc723417e61563
ultorhash <ultorhash@gmail.com>
2025-07-13 22:38:57 +0200

    feat contract deployment function

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,35 +1,56 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Fragment, useState, type JSX } from 'react'
+import { ethers } from 'ethers'
+import SimpleContract from './contracts/SimpleContract.json'
 
-function App() {
-  const [count, setCount] = useState(0)
+export const App = (): JSX.Element => {
+  const [contractAddress, setContractAddress] = useState<string | null>(null);
+  const [loading, setLoading] = useState(false);
+
+  const deployContract = async () => {
+    try {
+      setLoading(true);
+
+      if (!window.ethereum) {
+        alert("Zainstaluj Metamask");
+        return;
+      }
+
+      await window.ethereum.request({ method: "eth_requestAccounts" });
+      const provider = new ethers.JsonRpcProvider(window.ethereum);
+      const signer = await provider.getSigner();
+
+      const factory = new ethers.ContractFactory(
+        SimpleContract.abi,
+        SimpleContract.bytecode,
+        signer
+      );
+
+      const contract = await factory.deploy();
+      await contract.waitForDeployment();
+      const address = await contract.getAddress()
+
+      setContractAddress(address);
+      console.log("Contract deployed at:", address);
+    } catch (error) {
+      console.error("Deploy error:", error);
+      alert("Deploy nieudany, sprawdź konsolę.");
+    } finally {
+      setLoading(false);
+    }
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+    <Fragment>
+      <h1>Hello</h1>
+      <button onClick={deployContract} disabled={loading}>
+        {loading ? "Deploying..." : "Deploy Contract"}
+      </button>
+
+      {contractAddress && (
         <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
+          Contract deployed at: <strong>{contractAddress}</strong>
         </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+      )}
+    </Fragment>
   )
 }
-
-export default App
