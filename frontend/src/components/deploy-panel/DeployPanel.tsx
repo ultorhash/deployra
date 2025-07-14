c36401a2d725d08403ae119aeb1efe33427d97c0
ultorhash <ultorhash@gmail.com>
2025-07-14 23:23:11 +0200

    feat snackbar messages on deploy status

--- frontend/src/components/deploy-panel/DeployPanel.tsx
+++ frontend/src/components/deploy-panel/DeployPanel.tsx
@@ -1,13 +1,13 @@
-import { Fragment, useEffect, useState, type JSX } from "react";
-import { Avatar, Box, Button, Card, CardActions, CardContent, CardHeader, Tab, Tabs, TextField, Tooltip } from "@mui/material";
+import { Fragment, useState, type JSX } from "react";
+import { Avatar, Box, Button, Card, CardActions, CardContent, CardHeader, Snackbar, Tab, Tabs, TextField, Tooltip } from "@mui/material";
 import { FieldValues, useForm } from "react-hook-form";
+import MuiAlert from '@mui/material/Alert';
 import { useChainId,
   useChains,
   useSwitchChain,
   useAccount,
   useConnect,
-  injected,
-  useWriteContract
+  injected
 } from "wagmi";
 import { sepolia } from "viem/chains";
 import { DeployOption } from "@app-types";
@@ -34,18 +34,39 @@ export const DeployPanel = (): JSX.Element => {
   const [selectedOption, setSelectedOption] = useState<DeployOption>();
   const [isPending, setIsPending] = useState<boolean>(false);
 
+  const [snackbar, setSnackbar] = useState({
+    open: false,
+    message: "",
+    status: ""
+  });
+
   const handleClick = (option: DeployOption) => {
+    console.log(chainId);
+    console.log(sepolia.id);
+
+    if (chainId !== option.chainId) {
+      switchChainAsync({ chainId: option.chainId });
+    }
+  
     setSelectedOption(option);
   };
 
-  const handleChange = (event: React.SyntheticEvent, newValue: number) => {
+  const getButtonText = (): string => {
+    if (!isConnected) return 'Connect wallet';
+    return isPending ? 'Deploying...' : 'Deploy';
+  }
+
+  const handleChange = (_: React.SyntheticEvent, newValue: number) => {
     setValue(newValue);
   }
 
-  const onSubmit = async (data: FieldValues): Promise<void> => {
+  const onSubmit = async (formData: FieldValues): Promise<void> => {
     setIsPending(true);
-    console.log(chainId);
-    console.log(sepolia.id);
+    setSnackbar({
+      open: true,
+      message: 'Deploying...',
+      status: 'waiting',
+    });
 
     const provider = new ethers.BrowserProvider(window.ethereum);
     const signer = await provider.getSigner();
@@ -57,14 +78,22 @@ export const DeployPanel = (): JSX.Element => {
     );
 
     try {
-      const contract = await factory.deploy(data.name, data.symbol);
+      const contract = await factory.deploy(formData.name, formData.symbol);
       console.log("Deploying contract...");
+      const txHash = contract.deploymentTransaction()?.hash;
 
       await contract.waitForDeployment();
       const address = await contract.getAddress();
 
       console.log("Token deployed at:", address);
-      alert(`Token deployed at: ${address}`);
+      console.log("Tx hash:", txHash);
+      
+      setSnackbar({
+        open: true,
+        message: "Token deployed successfully!",
+        status: "success"
+      });
+
     } catch (err) {
       console.error("Deploy error:", err);
     } finally {
@@ -89,133 +118,151 @@ export const DeployPanel = (): JSX.Element => {
   }
 
   return (
-    <Card sx={(theme) => ({
-      p: 2,
-      borderRadius: 2
-    })}>
-      <CardHeader
-        title={`Deploy your smart contract`}
-        subheader={selectedOption?.chain ? `Selected chain: ${selectedOption?.chain}` : 'Please select chain'}
-        sx={{ textAlign: "center" }}
-      />
-      <CardContent>
-        <Box
-          display="flex"
-          flexDirection="column"
-          gap={2}
-        >
-          {rows.map((row: DeployOption[], rowIndex: number) => (
-            <Box display="flex" key={rowIndex} gap={2}>
-              {row.map((option: DeployOption, index: number) => (
-                <Box key={index} width={44} height={44}>
-                  <Tooltip
-                    arrow
-                    title={
-                      <Fragment>
-                        <p style={{ margin: 0 }}>{option.chain}</p>
-                        <p style={{ margin: 0 }}>Fee: {option.fee}</p>
-                      </Fragment>
-                    }
-                    slotProps={{
-                      transition: {
-                        timeout: 0
+    <Fragment>
+      <Card sx={(theme) => ({
+        p: 2,
+        borderRadius: 2
+      })}>
+        <CardHeader
+          title={`Deploy your smart contract`}
+          subheader={selectedOption?.chain ? `Selected chain: ${selectedOption?.chain}` : 'Please select chain'}
+          sx={{ textAlign: "center" }}
+        />
+        <CardContent>
+          <Box
+            display="flex"
+            flexDirection="column"
+            gap={2}
+          >
+            {rows.map((row: DeployOption[], rowIndex: number) => (
+              <Box display="flex" key={rowIndex} gap={2}>
+                {row.map((option: DeployOption, index: number) => (
+                  <Box key={index} width={44} height={44}>
+                    <Tooltip
+                      arrow
+                      title={
+                        <Fragment>
+                          <p style={{ margin: 0 }}>{option.chain}</p>
+                          <p style={{ margin: 0 }}>Fee: {option.fee}</p>
+                        </Fragment>
                       }
-                    }}
-                  >
-                    <Avatar
-                      alt={option.chain}
-                      src={`/assets/logos/${option.icon}`}
-                      onClick={() => handleClick(option)}
-                      sx={{
-                        width: 44,
-                        height: 44,
-                        cursor: "pointer",
-                        transition: "0.1s",
-                        opacity: selectedOption?.chain === option.chain ? 1 : 0.4,
-                        boxShadow: selectedOption?.chain === option.chain ? "0 0 8px #000" : "none"
+                      slotProps={{
+                        transition: {
+                          timeout: 0
+                        }
                       }}
-                    />
-                  </Tooltip>
-                </Box>
-              ))}
-            </Box>
-          ))}
-        </Box>
-      </CardContent>
-      <CardActions>
-        <Box sx={{ width: '100%' }}>
-          <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
-            <Tabs
-              aria-label="deploy type"
-              value={value}
-              onChange={handleChange}
-              sx={(theme) => ({
-                '& .Mui-selected': { color: theme.palette.text.primary },
-                '& .MuiTabs-indicator': { backgroundColor: theme.palette.text.primary }
-              })}
-            >
-              <Tab label="Token" />
-              <Tab label="Contract" />
-            </Tabs>
+                    >
+                      <Avatar
+                        alt={option.chain}
+                        src={`/assets/logos/${option.icon}`}
+                        onClick={() => handleClick(option)}
+                        sx={{
+                          width: 44,
+                          height: 44,
+                          cursor: "pointer",
+                          transition: "0.1s",
+                          opacity: selectedOption?.chain === option.chain ? 1 : 0.4,
+                          boxShadow: selectedOption?.chain === option.chain ? "0 0 8px #000" : "none"
+                        }}
+                      />
+                    </Tooltip>
+                  </Box>
+                ))}
+              </Box>
+            ))}
           </Box>
-          <CustomTabPanel value={value} index={0}>
-            <form
-              noValidate
-              method="post"
-              onSubmit={(e) => {
-                e.preventDefault();
-                isConnected ? handleSubmit(onSubmit)() : connect({ connector: injected() })
-              }}
-            >
-              <Box
-                sx={{
-                  display: 'flex',
-                  gap: 2,
-                  mb: 2
+        </CardContent>
+        <CardActions>
+          <Box sx={{ width: '100%' }}>
+            <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
+              <Tabs
+                aria-label="deploy type"
+                value={value}
+                onChange={handleChange}
+                sx={(theme) => ({
+                  '& .Mui-selected': { color: theme.palette.text.primary },
+                  '& .MuiTabs-indicator': { backgroundColor: theme.palette.text.primary }
+                })}
+              >
+                <Tab label="Token" />
+                <Tab label="Contract" />
+              </Tabs>
+            </Box>
+            <CustomTabPanel value={value} index={0}>
+              <form
+                noValidate
+                method="post"
+                onSubmit={(e) => {
+                  e.preventDefault();
+                  isConnected ? handleSubmit(onSubmit)() : connect({ connector: injected() })
                 }}
               >
-                <TextField
-                  fullWidth
-                  label="Name"
-                  variant="outlined"
-                  autoComplete="off"
-                  {...register('name', { required: 'name is required' })}
-                  error={!!errors.name}
-                  helperText={errors.name && typeof errors.name.message === 'string' ? errors.name.message : ''}
-                />
-                <TextField
-                  fullWidth
-                  label="Symbol"
-                  variant="outlined"
-                  autoComplete="off"
-                  {...register('symbol', { required: 'symbol is required' })}
-                  error={!!errors.symbol}
-                  helperText={errors.symbol && typeof errors.symbol.message === 'string' ? errors.symbol.message : ''}
-                />
-              </Box>
+                <Box
+                  sx={{
+                    display: 'flex',
+                    gap: 2,
+                    mb: 2
+                  }}
+                >
+                  <TextField
+                    fullWidth
+                    label="Name"
+                    variant="outlined"
+                    autoComplete="off"
+                    {...register('name', { required: 'name is required' })}
+                    error={!!errors.name}
+                    helperText={errors.name && typeof errors.name.message === 'string' ? errors.name.message : ''}
+                  />
+                  <TextField
+                    fullWidth
+                    label="Symbol"
+                    variant="outlined"
+                    autoComplete="off"
+                    {...register('symbol', { required: 'symbol is required' })}
+                    error={!!errors.symbol}
+                    helperText={errors.symbol && typeof errors.symbol.message === 'string' ? errors.symbol.message : ''}
+                  />
+                </Box>
 
-              <Button
-                fullWidth
-                type="submit"
-                variant="contained"
-                color="primary"
-                disabled={isPending}
-              >
-                {
-                  isPending
-                    ? "Deploying..."
-                    : isConnected
-                    ? "Deploy"
-                    : "Connect wallet"
-                }
-              </Button>
-            </form>
-          </CustomTabPanel>
-          <CustomTabPanel value={value} index={1}>
-            Item two
-          </CustomTabPanel>
-        </Box>
-      </CardActions>
-    </Card>
+                <Button
+                  fullWidth
+                  type="submit"
+                  variant="contained"
+                  color="primary"
+                  disabled={isPending}
+                >
+                  {getButtonText()}
+                </Button>
+              </form>
+            </CustomTabPanel>
+            <CustomTabPanel value={value} index={1}>
+              Item two
+            </CustomTabPanel>
+          </Box>
+        </CardActions>
+      </Card>
+      <Snackbar
+        open={snackbar.open}
+        autoHideDuration={5000}
+        onClose={() => setSnackbar((prev) => ({ ...prev, open: false }))}
+        message={snackbar.message}
+        anchorOrigin={{ vertical: 'bottom', horizontal: 'right' }}
+      >
+        <MuiAlert
+          elevation={6}
+          variant="filled"
+          onClose={() => setSnackbar((prev) => ({ ...prev, open: false }))}
+          sx={{ width: '100%' }}
+          severity={
+            snackbar.status === 'success'
+              ? 'success' : snackbar.status === 'error'
+              ? 'error' : snackbar.status === 'waiting'
+              ? 'info' : undefined
+          }
+        >
+          {snackbar.message}
+        </MuiAlert>
+      </Snackbar>
+    </Fragment>
   );
 };
