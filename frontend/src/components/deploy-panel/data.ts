701115fdc70c1760ac32aab58b64851401f9071d
ultorhash <ultorhash@gmail.com>
2025-07-15 22:36:57 +0200

    chore adjust fees for chains

--- frontend/src/components/deploy-panel/data.ts
+++ frontend/src/components/deploy-panel/data.ts
@@ -5,85 +5,85 @@ export const deployOptions: DeployOption[] = [
     chain: "Base",
     chainId: 8453,
     icon: "base.png",
-    fee: 0.000005
+    fee: 0.000015
   },
   {
     chain: "Optimism",
     chainId: 10,
     icon: "optimism.png",
-    fee: 0.000005
+    fee: 0.000015
   },
   {
     chain: "Unichain",
     chainId: 130,
     icon: "unichain.png",
-    fee: 0.000005
+    fee: 0.000015
   },
   {
     chain: "Soneium",
     chainId: 1868,
     icon: "soneium.svg",
-    fee: 0.000005
+    fee: 0.000015
   },
   {
     chain: "Sonic",
     chainId: 146,
     icon: "sonic.svg",
-    fee: 0.000005
+    fee: 0.000015
   },
   {
     chain: "Polygon",
     chainId: 137,
     icon: "polygon.svg",
-    fee: 0.000005
+    fee: 0.000015
   },
   {
     chain: "Sei",
     chainId: 1329,
     icon: "sei.svg",
-    fee: 0.000005
+    fee: 0.000015
   },
   {
     chain: "Scroll",
     chainId: 534352,
     icon: "scroll.png",
-    fee: 0.000005
+    fee: 0.000015
   },
   {
     chain: "Lens",
     chainId: 232,
     icon: "lens.svg",
-    fee: 0.000005
+    fee: 0.000015
   },
   {
     chain: "Linea",
     chainId: 59144,
     icon: "linea.png",
-    fee: 0.000005
+    fee: 0.000015
   },
   {
     chain: "Morph",
     chainId: 2818,
     icon: "morph.svg",
-    fee: 0.000005
+    fee: 0.000015
   },
   {
     chain: "Berachain",
     chainId: 80085,
     icon: "berachain.svg",
-    fee: 0.000005
+    fee: 0.000015
   },
   {
     chain: "Abstract",
     chainId: 2741,
     icon: "abstract.png",
-    fee: 0.000005
+    fee: 0.000015
   },
   {
     chain: "Mantle",
     chainId: 5000,
     icon: "mantle.svg",
-    fee: 0.000005
+    fee: 0.000015
   },
   {
     chain: "Sepolia Testnet",
