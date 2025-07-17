c020ce613da2efdc724aaf7572efe91a07e6a498
ultorhash <ultorhash@gmail.com>
2025-07-17 21:08:22 +0200

    feat header subtitle

--- frontend/src/components/header/Header.tsx
+++ frontend/src/components/header/Header.tsx
@@ -1,26 +1,29 @@
 import type { JSX } from "react";
-import { AppBar, Box, Toolbar, Typography } from "@mui/material";
+import { AppBar, Box, Toolbar, Typography, useTheme } from "@mui/material";
 import { ConnectButton } from "@rainbow-me/rainbowkit";
 
 export const Header = (): JSX.Element => {
+  const theme = useTheme();
+
   return (
     <Box sx={{ flexGrow: 1 }}>
       <AppBar position="static">
         <Toolbar>
-          <Box sx={{ width: 32, height: 32, mr: 1 }}>
+          <Box sx={{ width: 44, height: 44, mr: 1 }}>
             <img
               src="/assets/icons/rocket.svg"
               alt="logo"
               style={{ width: '100%', height: '100%', objectFit: 'contain' }}
             />
           </Box>
-          <Typography
-            variant="h6"
-            component="div"
-            sx={{ flexGrow: 1 }}
-          >
-            Deployra
-          </Typography>
+           <Box sx={{ flexGrow: 1 }}>
+            <Typography variant="h6" component="p">
+              Deployra
+            </Typography>
+            <Typography variant="caption" component="p" sx={{ color: theme.palette.text.secondary }}>
+              Fast contract deployment
+            </Typography>
+          </Box>
           <ConnectButton chainStatus="icon" />
         </Toolbar>
       </AppBar>
