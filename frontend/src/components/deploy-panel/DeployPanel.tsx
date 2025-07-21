caf959da9ab3ed93d4ad6ecee70042f9e9702065
ultorhash <ultorhash@gmail.com>
2025-07-21 12:00:59 +0200

    chore remove old new chains

--- frontend/src/components/deploy-panel/DeployPanel.tsx
+++ frontend/src/components/deploy-panel/DeployPanel.tsx
@@ -220,7 +220,7 @@ export const DeployPanel = (): JSX.Element => {
                 }
                 slotProps={{ transition: { timeout: 0 } }}
               >
-                {option.chain === "HyperEVM" || option.chain === "Pharos Testnet" || option.chain === "Citrea Testnet" ? (
+                {false ? (
                   <Badge
                     badgeContent="New!"
                     anchorOrigin={{
