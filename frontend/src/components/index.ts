28a73bc040cddae59f52e3bd9a975c5a081fb0ae
ultorhash <ultorhash@gmail.com>
2025-12-04 19:26:45 +0100

    chore remove old component

--- frontend/src/components/index.ts
+++ frontend/src/components/index.ts
@@ -2,7 +2,6 @@ export { Header } from './header/Header';
 export { Sidebar } from './sidebar/Sidebar';
 export { Content } from './content/Content';
 export { DynamicForm } from './dynamic-form/DynamicForm';
-export { ChainTiles } from './chain-tiles/ChainTiles';
 export { Tile } from './tile/Tile';
 export { BaseLearnTile } from './base-learn-tile/BaseLearnTile';
 export { ReferralPanel } from './referral-panel/ReferralPanel';
